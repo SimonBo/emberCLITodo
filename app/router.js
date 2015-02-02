efe021256a86fa9029461a58e0559637ecf92db4
Szymon Borucki <szymon.borucki@gmail.com>
2015-02-02 16:24:01 +0800

    Initial and final commit

--- app/router.js
+++ app/router.js
@@ -1,11 +1,18 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
+  this.resource("todos", {
+    path: "/"
+  }, function() {
+    this.route("complete");
+    this.route("active");
+    
+  });
 });
 
 export default Router;
